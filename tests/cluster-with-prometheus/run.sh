ade2079613b86038b186da3b5ba606b615793c77
Oleg Strokachuk <oleg.strokachuk@vizor-games.com>
2021-08-28 02:39:21 +0300

    Add test kind cluster to check everything in grafana, prometheus and alertmanager

---
+++ tests/cluster-with-prometheus/run.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+set -e
+
+WORKDIR=$(realpath "$(dirname "$0")")
+REPO_DIR=$(realpath "${WORKDIR[*]}/../../")
+echo "WORKDIR ${WORKDIR}"
+echo "REPO_DIR ${REPO_DIR}"
+
+echo "Create kind cluster"
+kind create cluster -v 1 --config $WORKDIR/kind-config.yaml
+
+echo "Add chart repo and update index"
+helm repo add ingress-nginx https://kubernetes.github.io/ingress-nginx --force-update
+helm repo add prometheus-community https://prometheus-community.github.io/helm-charts
+helm repo update
+
+$WORKDIR/deploy.sh
