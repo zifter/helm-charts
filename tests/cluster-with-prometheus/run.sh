833e8a26c7b3e33a3ea985bbf56da31d454572f8
Oleg Strokachuk <oleg.strokachuk@vizor-games.com>
2021-08-28 22:01:18 +0300

    Add ingress to process

--- tests/cluster-with-prometheus/run.sh
+++ tests/cluster-with-prometheus/run.sh
@@ -2,16 +2,9 @@
 set -e
 
 WORKDIR=$(realpath "$(dirname "$0")")
-REPO_DIR=$(realpath "${WORKDIR[*]}/../../")
 echo "WORKDIR ${WORKDIR}"
-echo "REPO_DIR ${REPO_DIR}"
 
-echo "Create kind cluster"
-kind create cluster -v 1 --config $WORKDIR/kind-config.yaml
-
-echo "Add chart repo and update index"
-helm repo add ingress-nginx https://kubernetes.github.io/ingress-nginx --force-update
-helm repo add prometheus-community https://prometheus-community.github.io/helm-charts
-helm repo update
-
+$WORKDIR/setup.sh
 $WORKDIR/deploy.sh
+
+curl http://localhost:1080/domain-exporter/metrics
